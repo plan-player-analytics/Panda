69e68292d078d0cad2f7184905d01d333d26cbfc
Rsl1122 <24460436+Rsl1122@users.noreply.github.com>
2020-07-12 11:59:45 +0300

    Debugging mention filtering

--- modules/no_ping.js
+++ modules/no_ping.js
@@ -25,12 +25,12 @@ module.exports = function (client) {
         // Webhooks don't have members, and they are trusted to be respectful
         if (msg.webhookID != null) return;
 
-        // Staff can mention
-        if (isStaff(msg.member)) return;
+        // // Staff can mention
+        // if (isStaff(msg.member)) return;
 
         // Get the map of people who were mentioned in the message
         const mentioned = msg.mentions.members
-            .filter(member => !member.user.bot)        // Filter out bots, they don't have feelings
+            // .filter(member => !member.user.bot)        // Filter out bots, they don't have feelings
             .filter(member => member.id !== author.id) // Filter out users mentioning themselves
             // Ignore mentions of users with '@me' in their nick (include mentions of users with no '@me' in nick)
             .filter(member => !member.nickname || member.nickname.toLowerCase().indexOf("@me") === -1);
@@ -39,6 +39,7 @@ module.exports = function (client) {
         if (mentioned.size === 0) return;
 
         const currentDate = new Date().getTime();
+        console.log(`${msg.author.username} mentioned ${mentioned}`);
         channel
             .fetchMessages({before: msg.id, limit: 50})
             .then(filterRecent(currentDate))
@@ -53,6 +54,8 @@ module.exports = function (client) {
                     // Remove the user who sent a message from the list of mentioned users
                     mentioned.delete(msg.author.id);
                 })
+
+                console.log(`After filtering ${mentioned}`);
 
                 // The message author mentioned someone that hadn't sent a message in the given amount of time
                 if (mentioned.size > 0) {
